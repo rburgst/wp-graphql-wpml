5488f3be240719ccd01ec0d1c59cd41b164ff026
Rainer Burgstaller <rburgst@gmail.com>
2020-09-22 10:55:19 +0200

    start working on being able to get translated menu items
    - does not work properly yet

--- wp-graphql-wpml.php
+++ wp-graphql-wpml.php
@@ -1,4 +1,7 @@
 <?php
+
+use WPGraphQL\Data\Connection\AbstractConnectionResolver;
+
 /**
  * Plugin Name: WPGraphQL WPML
  * Plugin URI: https://github.com/rburgst/wp-graphql-wpml
@@ -99,7 +102,7 @@ function wpgraphqlwpml_add_post_type_fields(\WP_Post_Type $post_type_object)
                 }
 
                 list($thisPost, $translationUrl) = graphql_wpml_get_translation_url($post_id, $post_language);
-                
+
                 return $translationUrl;
             },
         ]
@@ -275,9 +278,139 @@ function wpgraphqlwpml_action_graphql_register_types()
             ],
         ],
     ]);
+
+    register_graphql_fields('RootQueryToMenuItemConnectionWhereArgs', [
+        'language' => [
+            'type' => 'String',
+            'description' => 'filters the menu items by language',
+        ],
+    ]);
+    register_graphql_fields('RootQueryToMenuConnectionWhereArgs', [
+        'language' => [
+            'type' => 'String',
+            'description' => 'filters the menus by language',
+        ],
+    ]);
     foreach (\WPGraphQL::get_allowed_post_types() as $post_type) {
         wpgraphqlwpml_add_post_type_fields(get_post_type_object($post_type));
     }
+}
+
+function wpgraphqlwpml__translate_menu_location(
+    string $location,
+    string $language
+): string
+{
+    global $sitepress;
+
+    $language = strtolower($language);
+    return "${location}___${language}";
+}
+
+function wpgraphqlwpml__theme_mod_nav_menu_locations(array $args) {
+    foreach ($args as $menu_name => $menu_term_id) {
+        $translated = get_term($menu_term_id);
+        $args[$menu_name] = $translated->term_id;
+    }
+    return $args;
+}
+
+function wpgraphqlwpml__filter_graphql_connection_query_args(array $args)
+{
+    global $sitepress;
+
+    if ($args['taxonomy'] !== 'nav_menu') {
+        return $args;
+    }
+    $target_lang = $args['language'];
+    if (!isset($target_lang)) {
+        return $args;
+    }
+
+
+    $curLang = $sitepress->get_current_language();
+    // Required only when using other than the default language because the
+    // menu location for the default language is the original location
+    if ($curLang !== $target_lang) {
+        $sitepress->switch_lang($target_lang);
+
+        if (!has_filter('theme_mod_nav_menu_locations', 'wpgraphqlwpml__theme_mod_nav_menu_locations')) {
+            add_filter('theme_mod_nav_menu_locations', 'wpgraphqlwpml__theme_mod_nav_menu_locations');
+        }
+//        $args['where']['location'] = wpgraphqlwpml__translate_menu_location(
+//            $args['where']['location'],
+//            $target_lang
+//        );
+    }
+
+    unset($target_lang);
+
+    return $args;
+}
+
+$wpgraphqlwpml_prev_language = null;
+
+function wpgraphqlwpml__filter_graphql_connection_should_execute(bool $should_execute, AbstractConnectionResolver $resolver)
+{
+    global $sitepress;
+    global $wpgraphqlwpml_prev_language;
+
+
+    $fieldName = $resolver->getInfo()->fieldName;
+    if ($fieldName === 'menuItems') {
+        $args = $resolver->getArgs();
+        if (!isset($args['where']) && isset($args['where']['language'])) {
+            return $should_execute;
+        }
+        $new_lang = $args['where']['language'];
+        $current_language = $sitepress->get_current_language();
+        if ($new_lang !== $current_language) {
+            $sitepress->switch_lang($new_lang);
+            $wpgraphqlwpml_prev_language = $current_language;
+        }
+        unset($args['where']['language']);
+    } else if ($fieldName === 'menus') {
+        $args = $resolver->getArgs();
+        if (!isset($args['where']) && isset($args['where']['language'])) {
+            return $should_execute;
+        }
+        $new_lang = $args['where']['language'];
+        $current_language = $sitepress->get_current_language();
+        if ($new_lang !== $current_language) {
+            $sitepress->switch_lang($new_lang);
+            $wpgraphqlwpml_prev_language = $current_language;
+        }
+        unset($args['where']['language']);
+    }
+    return $should_execute;
+}
+
+function wpgraphqlwpml__filter_graphql_connection_query(mixed $query, AbstractConnectionResolver $resolver)
+{
+    return $query;
+}
+
+function wpgraphqlwpml__filter_graphql_connection_ids(array $ids, AbstractConnectionResolver $resolver)
+{
+    global $sitepress;
+    global $wpgraphqlwpml_prev_language;
+
+    if ($resolver->getInfo()->fieldName === 'menus') {
+        $args = $resolver->getArgs();
+        if (!isset($args['where']) && !isset($wpgraphqlwpml_prev_language)) {
+            return $ids;
+        }
+        $result = array();
+        foreach ($ids as $orig_id) {
+            $translated = get_term($orig_id);
+            array_push($result, $translated->term_id);
+        }
+//        $sitepress->switch_lang($wpgraphqlwpml_prev_language);
+//        unset($wpgraphqlwpml_prev_language);
+        return $result;
+    }
+
+    return $ids;
 }
 
 function wpgraphqlwpml_action_init()
@@ -290,6 +423,31 @@ function wpgraphqlwpml_action_init()
         'wpgraphqlwpml_action_graphql_register_types',
         10,
         0
+    );
+
+    add_filter(
+        'graphql_connection_query_args',
+        'wpgraphqlwpml__filter_graphql_connection_query_args',
+        10,
+        2
+    );
+    add_filter(
+        'graphql_connection_should_execute',
+        'wpgraphqlwpml__filter_graphql_connection_should_execute',
+        10,
+        2
+    );
+//    add_filter(
+//        'graphql_connection_query',
+//        'wpgraphqlwpml__filter_graphql_connection_query',
+//        10,
+//        2
+//    );
+    add_filter(
+        'graphql_connection_ids',
+        'wpgraphqlwpml__filter_graphql_connection_ids',
+        10,
+        2
     );
 }
 
